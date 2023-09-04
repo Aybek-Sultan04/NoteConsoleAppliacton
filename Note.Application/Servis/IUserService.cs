a99de7a11ff186c2c5ec2054d7f0e748daa001c9
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-09-04 14:35:30 +0300

    Add Interface IUserService

---
+++ Note.Application/Servis/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Note.Domain.Models;
+
+namespace Note.Application.Servis
+{
+    public interface IUserService
+    {
+        Task<bool> CheckUserAsync(Guid UserId);
+        Task<bool> AddUserAsync(User user);
+        Task<bool> DeleteUserAsync(User user);
+        Task<bool> UpdateUserAsync(User user);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+    }
+}
